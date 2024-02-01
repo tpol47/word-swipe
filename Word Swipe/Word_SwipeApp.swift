664f3c01f388bd1fb06122d46b6ea037d063468e
Thomas Pollick <thomascpollick@gmail.com>
2024-02-01 12:07:12 -0600

    Initial Commit

---
+++ Word Swipe/Word_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_SwipeApp.swift
+//  Word Swipe
+//
+//  Created by Thomas Pollick on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
